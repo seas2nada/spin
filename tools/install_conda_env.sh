08482449506e99e892b487ca3b138f8cee8174b3
seas2nada <dooh1474@naver.com>
2023-08-28 11:42:07 +0900

    initial commit

---
+++ tools/install_conda_env.sh
@@ -0,0 +1,1 @@
+conda install pytorch==1.13.1 torchvision==0.14.1 torchaudio==0.13.1 pytorch-cuda=11.6 -c pytorch -c nvidia
---
+++ tools/install_flashlight.sh
@@ -0,0 +1,43 @@
+# Dependencies
+sudo apt install libfftw3-dev libopenmpi-dev
+sudo apt install build-essential cmake
+sudo apt install libboost-system-dev libboost-thread-dev libboost-program-options-dev libboost-test-dev libeigen3-dev
+sudo apt install zlib1g-dev libbz2-dev liblzma-dev
+sudo apt install pkg-config
+
+# activate venv
+. ./activate_python.sh
+
+# Install mkl
+sudo bash ./install_mkl.sh
+
+# Install arrayfire
+wget https://arrayfire.s3.amazonaws.com/3.8.0/ArrayFire-v3.8.0_Linux_x86_64.sh
+sudo chmod -R +777 ./ArrayFire-v3.8.0_Linux_x86_64.sh
+./ArrayFire-v3.8.0_Linux_x86_64.sh
+
+# Install flashlight
+# Piz install mkl before excute above lines
+rm -rf flashlight
+export PATH=/usr/local/cuda-11.3/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda/lib:${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+git clone https://github.com/flashlight/flashlight.git && cd flashlight
+git reset --hard 03c51129f320eed7ff0d416f7e8291a029439039
+mkdir -p build && cd build
+
+## install kenlm
+git clone https://github.com/kpu/kenlm.git
+cd kenlm
+mkdir -p build && cd build
+cmake .. 
+make -j 16
+
+# error "Could NOT find NCCL": https://docs.nvidia.com/deeplearning/nccl/install-guide/index.html
+cmake .. -DCMAKE_BUILD_TYPE=Release -DFL_BACKEND=CUDA \
+-DArrayFire_DIR=$PWD/../../arrayfire/share/ArrayFire/cmake/
+make -j$(nproc)
+sudo make install
+
+## flashlight binding
+cd ../../../../flashlight/bindings/python
+python3 setup.py install
